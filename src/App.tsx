a9f0bb54e6b65ea0f83711360ba0b2859babccbc
teenage1ove <kirillstars7@gmail.com>
2025-01-20 12:39:50 +0300

    update app and app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
-export function App() {
-  return <div>App</div>
+import  List from './components/List/List'
+import styles from './App.module.css'
+
+export const App = function () {
+	return <div className={styles.app}>
+    <h1 className={styles.app__title}>Список репозиториев</h1>
+    <List />
+  </div>
 }
