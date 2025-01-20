677bc468d44aac5640e72c87f8ad93928ad73619
teenage1ove <kirillstars7@gmail.com>
2025-01-20 12:40:02 +0300

    add rootStoreContext

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { RootStoreContext } from './store/root-store-context.ts'
+import RootStore from './store/root-store.ts'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<RootStoreContext.Provider value={new RootStore()}>
+			<App />
+		</RootStoreContext.Provider>
 	</React.StrictMode>
 )
